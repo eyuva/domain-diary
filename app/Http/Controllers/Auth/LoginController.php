41ca20782676d7cc35ed324d62e3648dd29cecc7
Vishal Sancheti <mail@vishal.pro>
2018-05-21 17:00:18 +0530

    authentication with role and status

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if($user->status != 1) {
+            $this->guard()->logout();
+            $request->session()->invalidate();
+            return redirect('/');
+        }
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->tinyInteger('role')->default(0)->comment('0.User 1.Admin');
+            $table->tinyInteger('status')->default(0)->comment('0.Pending 1.Active 2.Blocked');
             $table->rememberToken();
             $table->timestamps();
         });
